995a6a852670775a11081ae7d5e3b97bf618d916
aqmal pangestu <pangestuaqmal@gmail.com>
2019-05-07 03:44:54 +0700

    cuaca lagi

--- www/weather/js/index.js
+++ www/weather/js/index.js
@@ -8,6 +8,7 @@ let temperature = document.getElementById("temp");
 let humidity = document.getElementById("humidity-div");
 
 var x = document.getElementById("cuaca");
+navigator.geolocation.getCurrentPosition(showPosition);
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -16,13 +17,11 @@ function getLocation() {
   }
 }
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-
-    var latt = parseInt(position.coords.latitude);
-    var long = parseInt(position.coords.longitude);
-
-          findWeatherDetails();
+    // -6.921507, 107.617751 bdg
+    var latt = (position.coords.latitude)-0.7;
+    var long = (position.coords.longitude)+0.8;
+    console.log('Your latitude is :'+latt+' and longitude is '+long);
+      findWeatherDetails();
       
       function findWeatherDetails() {
           let searchLink = "https://api.openweathermap.org/data/2.5/weather?lat=" +latt+ "&lon="+long+ "&appid="+appKey;
@@ -35,6 +34,45 @@ function showPosition(position) {
         icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
         temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
         humidity.innerHTML = jsonObject.main.humidity + "%";
+
+        function degToCard (deg){
+          if (deg>11.25 && deg<33.75){
+            return "NNE";
+          }else if (deg>33.75 && deg<56.25){
+            return "ENE";
+          }else if (deg>56.25 && deg<78.75){
+            return "E";
+          }else if (deg>78.75 && deg<101.25){
+            return "ESE";
+          }else if (deg>101.25 && deg<123.75){
+            return "ESE";
+          }else if (deg>123.75 && deg<146.25){
+            return "SE";
+          }else if (deg>146.25 && deg<168.75){
+            return "SSE";
+          }else if (deg>168.75 && deg<191.25){
+            return "S";
+          }else if (deg>191.25 && deg<213.75){
+            return "SSW";
+          }else if (deg>213.75 && deg<236.25){
+            return "SW";
+          }else if (deg>236.25 && deg<258.75){
+            return "WSW";
+          }else if (deg>258.75 && deg<281.25){
+            return "W";
+          }else if (deg>281.25 && deg<303.75){
+            return "WNW";
+          }else if (deg>303.75 && deg<326.25){
+            return "NW";
+          }else if (deg>326.25 && deg<348.75){
+            return "NNW";
+          }else{
+            return "N"; 
+          }
+        }
+        var compas = degToCard(jsonObject.wind.deg);
+        x.innerHTML = "Wind Speed : "+jsonObject.wind.speed+" m/s"+"<br>"+
+        "Wind Direction : "+compas+" "+jsonObject.wind.deg+"°";
       }
       
       function httpRequestAsync(url, callback)
