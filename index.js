245de84134a00dd48b1e795544006ed2b978997b
aqmal pangestu <pangestuaqmal@gmail.com>
2019-05-06 23:10:58 +0700

    edit cuaca

--- index.js
+++ index.js
@@ -65,21 +65,3 @@ function parsingRawData (data) {
 		});
         return hasilParsing;
     }
-
-
-    // Real-time cuaca
-        // const request = require('request');
-        // const argv = require('yargs').argv;
-        // let apiKey = '9697e661a188cb4621c5a52a23ca3907';
-        // let city = argv.c || 'Bandung';
-        // let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-        // request(url, function (err, response, body) {
-        //     if (err) {
-        //         console.log('error:', error);
-        //     } else {
-        //         let weather = JSON.parse(body)
-        //         let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        //         console.log(message);
-        //     }
-        // });
